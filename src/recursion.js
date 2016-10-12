5dfe3605209d1b69b10cc93e7f00c0ca81ca18c0
a-faraz <anjumfaraz10@gmail.com>
2016-10-12 10:50:35 -0500

    arraySum

--- src/recursion.js
+++ src/recursion.js
@@ -38,8 +38,19 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // Example: arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
-	
+  var sum = 0;
+  if (typeof(array) === 'object') {
+    for(var i=0; i < array.length; i++) {
+      sum = sum + arraySum(array[i])    
+    }
+  } else {
+    sum = sum + array;
+  }
+  return sum;
+
 };
+
+
 
 // 4. Check if a number is even.
 var isEven = function(n) {
